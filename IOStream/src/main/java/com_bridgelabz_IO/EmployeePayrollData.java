da8eaf7f43debbf11c6983d7e3a049e4a8dd14e7
lalit-003 <gahlawatl7@gmail.com>
2020-10-13 20:32:37 +0530

    Added class employeePayroll data and services with method to read and write employee details

--- IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollData.java
+++ IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollData.java
@@ -1,21 +1,20 @@
 package com_bridgelabz_IO;
 
 public class EmployeePayrollData {
-	
+
 	int id;
-	String name ;
+	String name;
 	double salary;
-	
-	public EmployeePayrollData(int id,String name,double salary)
-	{
+
+	public EmployeePayrollData(int id, String name, double salary) {
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
 	}
-	
-        @Override
-        public String toString()
-        {
-        	return "id is : "+ id +", name is : " + name +", salary is : "+salary;        }
-	
+
+	@Override
+	public String toString() {
+		return "id is : " + id + ", name is : " + name + ", salary is : " + salary;
+	}
+
 }
--- IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollService.java
+++ IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollService.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 
 public class EmployeePayrollService {
 
-	
-	
-		public static void main(String[] args)
-	{
-		System.out.println("Welcome to Employee payroll problem");
+	public static EmployeePayrollData readEmployeeData(Scanner sc) {
+		System.out.println("Enter employee id(int) ");
+		int employeeId = sc.nextInt();
+		System.out.println("Enter employee salary(double) ");
+		double salary = sc.nextDouble();
+		System.out.println("Enter employee name(string) ");
+		String name = sc.next();
+		EmployeePayrollData employee = new EmployeePayrollData(employeeId, name, salary);
+		return employee;
 	}
-	
+
+	public static void writeEmployeeData(EmployeePayrollData displayEmployeeData) {
+		System.out.println("\nWriting employee payroll data :\n" + displayEmployeeData);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		EmployeePayrollData emp = readEmployeeData(sc);
+		writeEmployeeData(emp);
+	}
+
 }
