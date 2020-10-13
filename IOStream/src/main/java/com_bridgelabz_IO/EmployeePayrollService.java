0aa49a5836178cf07b08c638dca490a5ffd9a08b
lalit-003 <gahlawatl7@gmail.com>
2020-10-14 01:10:17 +0530

    Created an EmployeePayrollFileIOServices and added method to write employee details in a text file

--- IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollService.java
+++ IOStream/src/main/java/com_bridgelabz_IO/EmployeePayrollService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-	
+	public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
 	private static List<EmployeePayrollData> employeePayrollList;
 	
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
@@ -13,26 +13,39 @@ public class EmployeePayrollService {
 	}
 
 	public static void readEmployeeData(Scanner sc) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
 		System.out.println("Enter employee id(int) ");
 		int employeeId = sc.nextInt();
 		System.out.println("Enter employee salary(double) ");
 		double salary = sc.nextDouble();
 		System.out.println("Enter employee name(string) ");
 		String name = sc.next();
-		EmployeePayrollData employee = new EmployeePayrollData(employeeId, name, salary);
-		employeePayrollList.add(employee);
-	}
+		employeePayrollService.readEmployeeData(sc);
+//        employeePayrollService.writeEmployeeData();
+        employeePayrollService.writeEmployeeData(IOService.FILE_IO);	}
 
-	public static void writeEmployeeData() {
-		System.out.println("\nWriting employee payroll data :\n" + employeePayrollList);
-	}
+//	public static void writeEmployeeData() {
+//		System.out.println("\nWriting employee payroll data :\n" + employeePayrollList);
+//	}
 
-	public static void main(String[] args) {
-		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-		Scanner sc = new Scanner(System.in);
-		 readEmployeeData(sc);
-		writeEmployeeData();
+
+	public void writeEmployeeData(IOService serviceType) {
+		if (serviceType == IOService.CONSOLE_IO)
+			System.out.println("Writing Employee Payroll Roaster to console\n" + employeePayrollList);
+		else if (serviceType == IOService.FILE_IO) {
+			EmployeePayrollFileIOService  employeepayrollIO = new EmployeePayrollFileIOService();
+			
+		employeepayrollIO.writeData(employeePayrollList);
+		}
 	}
+	
+	
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		 readEmployeeData(sc);
+//		writeEmployeeData();
+//	}
 
 }
--- IOStream/src/main/java/com_bridgelabz_IO/TestingEmployeeService.java
+++ IOStream/src/main/java/com_bridgelabz_IO/TestingEmployeeService.java
@@ -1,5 +1,20 @@
 package com_bridgelabz_IO;
 
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+import com_bridgelabz_IO.EmployeePayrollService.IOService;
+
 public class TestingEmployeeService {
 
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] empArray = {
+				new EmployeePayrollData(1, "John", 100000),
+				new EmployeePayrollData(2, "Jay", 250000),
+				new EmployeePayrollData(3, "Roy", 300000)
+		};
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(empArray));
+		employeePayrollService.writeEmployeeData(IOService.FILE_IO);
+			}
 }
